ddff98f6cebb8ff26e681581c57b41e2fccca60d
JamesRedPanda <James.Lizamore@redacademy.co.za>
2024-07-10 15:48:48 +0200

    Add project files.

---
+++ Flowers/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿select * from Flowers;
+
+INSERT INTO Flowers(flowerName, flowerQuant, flowerBloomDate, flowerInSeason)
+VALUES 
+	('Rose', 10, '2024-06-15', 1),
+	('Tulip', 20, '2024-04-25', 1),
+	('Lily', 15, '2024-07-01', 1),
+	('Sunflower', 12, '2024-08-10', 1),
+	('Daisy', 8, '2024-05-10', 1);
+
